8a689bce82a99c62abff84501797d196b085519f
chckn9 <thomasyeoh9@gmail.com>
2025-05-27 17:01:36 -0700

    Edit script to support cam object

---
+++ scripts/saveImage.py
@@ -0,0 +1,33 @@
+'''
+Get snapshot of latest livestream and save it to /images folder
+'''
+
+# TODO:
+# 1. Change hardcoded url
+# 2. Change saved img location/name
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def save_image(cam):
+    url = cam['cam_url']
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Locate poster image url
+    scripts = soup.find_all("script")
+    img_url = None
+    for script in scripts:
+        if script.string and "posterURL=" in script.string:
+            match = re.search(r"posterURL\s*=\s*['\"](.*?)['\"]", script.string)
+            if match:
+                img_url = match.group(1)
+                break
+
+    # Fetch image from url and save
+    if img_url:
+        img_response = requests.get(img_url).content
+        f = open(f"images/{cam['id']}.jpg", "wb")
+        f.write(img_response)
+        f.close()
